1fae47e595bba9668993b61fc1a0198d486dc2e2
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-02 15:01:12 -0500

    added search by brand functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(() => {
 
     let typeInputBox = $('#input-box-type');
+    let brandInputBox = $('#input-box-brand')
     let $brand = $('#brand')
     let $prod = $('#name')
     let $price = $('#price')
@@ -9,7 +10,7 @@ $(() => {
         $('#container').append(parentDiv)
 
 
- $('form').on('submit', (event) =>{
+ $('#type-form').on('submit', (event) =>{
 
     event.preventDefault();
 
@@ -64,13 +65,69 @@ $(() => {
         },
         () => {
             console.log('something went wrong')
-        }
-    )
+        })
 
+    })  
 
+    $('#brand-form').on('submit', (event) =>{
 
-
-})
-   
+        event.preventDefault();
+    
+        parentDiv.empty();
+    
+        const userInputBrand = $('#input-box-brand').val();
+        console.log('user input is', userInputBrand);
+    
+        $.ajax({
+            url: `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${userInputBrand}`
+        }).then(
+            data => {
+                
+    
+                    data.map(x => { 
+    
+                        
+                        let $prodDiv = $('<div>').addClass('product')
+    
+                        let brandDiv = $('<div>').addClass('brands')
+                        let nameDiv = $('<div>').addClass('names');
+                        let priceDiv = $('<div>').addClass('prices');
+                        let imageDiv = $('<div>').addClass('images');
+                        let imgUrl = $('<img>');
+    
+                
+    
+                    if (x.price !== '0.0'){
+    
+                    brandDiv.html(x.brand)
+                    $brand.append(brandDiv)
+                    
+                    nameDiv.html(x.name) 
+                    $prod.append(nameDiv)
+    
+                    priceDiv.html(x.price)
+                    $price.append(priceDiv)
+                    
+                    imgUrl.attr('src', x.image_link)
+                    $image.append(imageDiv)
+                    imageDiv.append(imgUrl)
+    
+                    $("img").on("error", function () {
+                        $(this).attr("src", "https://www.warnersstellian.com/Content/images/product_image_not_available.png");
+                    });
+    
+                    $prodDiv.append(brandDiv, nameDiv, priceDiv, imageDiv)
+                    parentDiv.append($prodDiv)
+    
+                    brandInputBox.val('')
+                    }
+                });
+            },
+            () => {
+                console.log('something went wrong')
+            })
+    
+        })  
+    
 
 })
